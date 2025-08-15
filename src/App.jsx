194476f9188bd75721877dc366e64fcbfb0a94ca
Anish Bauri <anishbauri984@gmail.com>
2025-08-15 13:15:52 +0530

    Setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-800 text-3xl font-bold text-slate-100">
+        Task Board
+      </h1>
+    </>
+  );
+}
+
+export default App;
